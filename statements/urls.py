cbe0e32c30bc94b31010e1918e2f61c4466df1c9
Jayachandra M <jayachandram@MacBook-Pro-von-Jayachandra.local>
2024-03-29 00:27:39 +0530

    Add tags

--- statements/urls.py
+++ statements/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import StatementsDetailView, StatementsListView, UploadFileView
+from .views import (
+    StatementsDetailView,
+    StatementsListView,
+    UploadFileView,
+    TagsSummaryView,
+)
 
 urlpatterns = [
     path("", StatementsListView.as_view(), name="statements-list"),
@@ -7,6 +12,11 @@ urlpatterns = [
         "transactions/<uuid:unique_id>",
         StatementsDetailView.as_view(),
         name="transactions-list",
+    ),
+    path(
+        "transactions/<uuid:unique_id>/tags",
+        TagsSummaryView.as_view(),
+        name="transactions-tags",
     ),
     path("upload", UploadFileView.as_view(), name="upload-statements"),
 ]
--- summary/urls.py
+++ summary/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BalanceSummaryView
+from .views import BalanceSummaryView, TagsSummaryView
 
 urlpatterns = [
     path("balance", BalanceSummaryView.as_view(), name="balance-summary"),
+    path("tags", TagsSummaryView.as_view(), name="tags-summary"),
 ]
